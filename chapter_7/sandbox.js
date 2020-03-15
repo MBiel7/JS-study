95ca096d6a4986d829f0509870e8e696ef2fc34a
Maciej Bielicki <mbielicki123@gmail.com>
2020-03-15 15:27:57 +0100

    finished chapter 7

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,6 +1,55 @@
 const form = document.querySelector('.signup-form');
-//const username = document.querySelector('#username')
+//const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/
+
+
 form.addEventListener('submit', e =>{
   e.preventDefault();
   console.log(form.username.value)
+
+  const username = form.username.value;
+
+  if(usernamePattern.test(username)){
+    feedback.textContent = 'That username is valid';
+  } else {
+    feedback.textContent = 'That username is not valid';
+  }
+
+
+})
+
+// const username = '1shaunaaa';
+// const pattern = /^[a-z]{6,}$/
+
+//let result = pattern.test(username);
+
+// console.log(result);
+
+// if(result){
+//   console.log('regex test passed');
+// } else {
+//   console.log('test failed');
+// }
+
+// let result = username.search(pattern);
+
+// console.log(result);
+
+
+
+form.username.addEventListener('keyup', e =>{
+
+  console.log(e.target.value);
+
+
+  
+
+  if(usernamePattern.test(e.target.value)){
+    form.username.setAttribute('class', 'succes');
+  } else {
+    form.username.setAttribute('class', 'error');
+  }
+
+
 })
