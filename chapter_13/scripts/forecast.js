978a29c2d30e18c793353b308b1529e48b51d799
Maciej Bielicki <mbielicki123@gmail.com>
2020-04-12 20:05:48 +0200

    Finished weather app

---
+++ chapter_13/scripts/forecast.js
@@ -0,0 +1,31 @@
+const key = '4z3SSrDV1C2I9c8GSEOAC40GxpJl4iAW';
+
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query)
+
+  const data = await response.json();
+  return data[0];
+}
+
+const getWeather = async (id) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query)
+
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// getCity('warsaw')
+//   .then(data => {return getWeather(data.Key)})
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err))
+
